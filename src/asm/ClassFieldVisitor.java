872175b4e6c91578688f29a0f27f6edd22cddae1
Matthew Persing <persinme@rose-hulman.edu>
2015-12-16 10:46:43 -0500

    ASM parser to internal representation should be complete

--- src/asm/ClassFieldVisitor.java
+++ src/asm/ClassFieldVisitor.java
@@ -1,12 +1,27 @@
 package asm;
 
-import data.api.IClass;
+import data.api.IField;
+import data.impl.Field;
 import jdk.internal.org.objectweb.asm.ClassVisitor;
+import jdk.internal.org.objectweb.asm.FieldVisitor;
+import jdk.internal.org.objectweb.asm.Type;
 
 public class ClassFieldVisitor extends ClassInformationVisitor {
 
 	public ClassFieldVisitor(int arg0, ClassVisitor arg1) {
 		super(arg0, arg1);
+	}
+	
+	@Override
+	public FieldVisitor visitField(int access, String name, String desc, String signature, 
+			Object value) {// value is null if no default value 
+		FieldVisitor toDecorate = super.visitField(access, name, desc, signature, value);
+		IField field = new Field();
+		field.setAccess(access);
+		field.setType(Type.getType(desc).getClassName());
+		field.setName(name);
+		newClass.addField(field);
+		return toDecorate;
 	}
 
 }
--- src/asm/ClassMethodVisitor.java
+++ src/asm/ClassMethodVisitor.java
@@ -1,12 +1,35 @@
 package asm;
 
-import data.api.IClass;
+import data.api.IMethod;
+import data.impl.Method;
 import jdk.internal.org.objectweb.asm.ClassVisitor;
+import jdk.internal.org.objectweb.asm.MethodVisitor;
+import jdk.internal.org.objectweb.asm.Type;
 
 public class ClassMethodVisitor extends ClassInformationVisitor {
 
 	public ClassMethodVisitor(int arg0, ClassVisitor arg1) {
 		super(arg0, arg1);
+	}
+	
+	@Override
+	public MethodVisitor visitMethod(int access, String name, String desc, String signature,
+			String[] exceptions) {
+		MethodVisitor toDecorateLaterThisQuarter = super.visitMethod(access, name, desc, signature
+				, exceptions);
+		IMethod method = new Method();
+		
+		Type[] argTypes = Type.getArgumentTypes(desc);
+		String[] classNames = new String[argTypes.length];
+		for(int i = 0 ; i < argTypes.length ; ++i) {
+			classNames[i] = argTypes[i].getClassName();
+		}
+		method.setArguments(classNames);
+		method.setReturnType(Type.getReturnType(desc).getClassName());
+		method.setName(name);
+		method.setAccess(access);
+		
+		return toDecorateLaterThisQuarter;
 	}
 
 
