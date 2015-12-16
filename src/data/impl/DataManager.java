50b0b77d890ca7a7d5101d2b7ee1af8e0347bed4
Matthew Persing <persinme@rose-hulman.edu>
2015-12-15 22:29:52 -0500

    Added creation of ClassVisitors in the DataManager (ASM ClassVisitors, not the visitor.impl ones.. we need to fix that)

--- src/data/impl/DataManager.java
+++ src/data/impl/DataManager.java
@@ -1,11 +1,19 @@
 package data.impl;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
-import visitor.api.ITraverser;
-import visitor.impl.OutputVisitor;
+import com.sun.xml.internal.ws.org.objectweb.asm.ClassReader;
+import com.sun.xml.internal.ws.org.objectweb.asm.ClassVisitor;
+
+import asm.ClassDeclarationVisitor;
+import asm.ClassFieldVisitor;
+import asm.ClassMethodVisitor;
 import data.api.IClass;
 import data.api.IDataManager;
+import jdk.internal.org.objectweb.asm.Opcodes;
+import visitor.api.ITraverser;
+import visitor.impl.OutputVisitor;
 
 public class DataManager implements IDataManager, ITraverser {
 
@@ -15,8 +23,21 @@ public class DataManager implements IDataManager, ITraverser {
 		this.classes = new ArrayList<IClass>();
 	}
 	
-	public void addClass(IClass c){
-		this.classes.add(c);
+	public void add(String toAdd) throws IOException{
+		IClass newClass = new Class();
+		classes.add(newClass);
+		ClassReader reader = new ClassReader(toAdd);
+		
+		ClassDeclarationVisitor declVisitor = new ClassDeclarationVisitor(Opcodes.ASM5);
+		declVisitor.setClass(newClass);
+		
+		ClassFieldVisitor fieldVisitor = new ClassFieldVisitor(Opcodes.ASM5, declVisitor);
+		fieldVisitor.setClass(newClass);
+		
+		ClassMethodVisitor methodVisitor = new ClassMethodVisitor(Opcodes.ASM5, fieldVisitor);
+		methodVisitor.setClass(newClass);
+		
+		reader.accept((ClassVisitor)methodVisitor, ClassReader.EXPAND_FRAMES);
 	}
 	
 	@Override
