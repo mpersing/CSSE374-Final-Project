63bd7f984072686dd46b0b11aca7a49ddd55962e
Tyler Whitehouse <whitehts@rose-hulman.edu>
2015-12-17 09:26:27 -0500

    File output to test.dot

--- src/runners/CommandLineRunner.java
+++ src/runners/CommandLineRunner.java
@@ -1,5 +1,8 @@
 package runners;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 import visitor.impl.ClassOutputVisitor;
@@ -12,7 +15,6 @@ public class CommandLineRunner {
 
 	public static void main(String[] args) throws IOException {
 		IDataManager data = new DataManager();
-		// TODO: add output visitors here
 		data.addOutputVisitor(new ClassOutputVisitor());
 		data.addOutputVisitor(new ExtendOutputVisitor());
 		data.addOutputVisitor(new ImplementOutputVisitor());
@@ -22,9 +24,20 @@ public class CommandLineRunner {
 		}
 		
 		StringBuffer sb = new StringBuffer();
-		// TODO: call output		
 		data.output(sb);
 		
 		System.out.println(sb.toString());
+		
+		BufferedWriter bwr = new BufferedWriter(new FileWriter(new File("test.dot")));
+        
+        //write contents of StringBuffer to a file
+        bwr.write(sb.toString());
+       
+        //flush the stream
+        bwr.flush();
+       
+        //close the stream
+        bwr.close();
+       
 	}
 }
