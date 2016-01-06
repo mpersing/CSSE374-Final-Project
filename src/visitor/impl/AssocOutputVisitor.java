766c2e1b159ca1082b77b479a2ea666839583a5d
Matthew Persing <persinme@rose-hulman.edu>
2016-01-05 20:29:58 -0500

    INSANITY (uses and associated mostly working?)

--- src/visitor/impl/AssocOutputVisitor.java
+++ src/visitor/impl/AssocOutputVisitor.java
@@ -1,5 +1,38 @@
 package visitor.impl;
 
+import data.api.IClass;
+import data.api.IDataManager;
+
 public class AssocOutputVisitor extends OutputVisitor {
 
+	/* Format:
+        edge [
+                arrowhead = "empty"
+        ]
+	 */
+	@Override
+	public void visit(IDataManager d){
+		this.sb.append("\n        edge [\n                style = \"solid\";\n                arrowhead = \"vee\"\n        ]\n");
+	}
+	
+	/* Format:
+	   Dog -> Animal
+       Cat -> Animal
+	 */
+	@Override
+	public void visit(IClass c) {
+		
+		// If the class composes or aggregates anything
+		for (String e : c.getAssoc()){
+			this.sb.append("        ");
+			this.sb.append("\"");
+			this.sb.append(c.getName());
+			this.sb.append("\"");
+			this.sb.append(" -> ");
+			this.sb.append("\"");
+			this.sb.append(e);
+			this.sb.append("\"");
+			this.sb.append("\n");
+		}
+	}
 }
--- src/visitor/impl/UsesOutputVisitor.java
+++ src/visitor/impl/UsesOutputVisitor.java
@@ -1,5 +1,36 @@
 package visitor.impl;
 
-public class UsesOutputVisitor extends OutputVisitor {
+import data.api.IClass;
+import data.api.IDataManager;
 
+public class UsesOutputVisitor extends OutputVisitor {
+	/* Format:
+    edge [
+            arrowhead = "empty"
+    ]
+ */
+@Override
+public void visit(IDataManager d){
+	this.sb.append("\n        edge [\n                style = \"dashed\";\n                arrowhead = \"vee\"\n        ]\n");
+}
+
+/* Format:
+   Dog -> Animal
+   Cat -> Animal
+ */
+@Override
+public void visit(IClass c) {
+	// If the class uses anything
+	for (String e : c.getUses()){
+		this.sb.append("        ");
+		this.sb.append("\"");
+		this.sb.append(c.getName());
+		this.sb.append("\"");
+		this.sb.append(" -> ");
+		this.sb.append("\"");
+		this.sb.append(e);
+		this.sb.append("\"");
+		this.sb.append("\n");
+	}
+}
 }
