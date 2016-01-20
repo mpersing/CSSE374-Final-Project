36e7b08200c75f7932220a2d391a29ecb705d403
Matthew Persing <persinme@rose-hulman.edu>
2016-01-19 21:03:47 -0500

    I think I can find singletons now?

--- src/pattern/api/IPatternFinder.java
+++ src/pattern/api/IPatternFinder.java
@@ -1,5 +1,12 @@
 package pattern.api;
 
+import java.util.Map;
+
+import data.api.IClass;
+import data.api.IUMLModifierManager;
+
 public interface IPatternFinder {
+	
+	public void find(Map<String, IClass> classMap, IUMLModifierManager mm);
 
 }
--- src/pattern/impl/SingletonPatternFinder.java
+++ src/pattern/impl/SingletonPatternFinder.java
@@ -1,7 +1,41 @@
 package pattern.impl;
 
+import java.util.Map;
+
+import data.api.IClass;
+import data.api.IField;
+import data.api.IMethod;
+import data.api.IUMLModifierManager;
 import pattern.api.IPatternFinder;
 
-public class SingletonPatternFinder implements IPatternFinder{
+public class SingletonPatternFinder implements IPatternFinder {
+
+	@Override
+	public void find(Map<String, IClass> classMap, IUMLModifierManager mm) {
+		String cName;
+		boolean foundField;
+		boolean foundMethod;
+		for(IClass c : classMap.values()) {
+			foundField = false;
+			foundMethod = false;
+			cName = c.getName();
+			for(IMethod m : c.getMethods()) {
+				if(m.getReturnType().equals(cName) && m.isStatic() && m.isPublic()) {
+					foundField = true;
+					break;
+				}
+			}
+			for(IField f : c.getFields()) {
+				if(f.getType().equals(cName) && f.isPrivate() && f.isStatic()) {
+					foundMethod = true;
+					break;
+				}
+			}
+			if(foundMethod && foundField) {
+				mm.setSubtext(cName, "\\<\\<Singleton\\>\\>");
+				mm.addStyle(cName, "color=blue");
+			}
+		}
+	}
 
 }
