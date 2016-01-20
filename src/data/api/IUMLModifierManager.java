c4e509040862d05e90d548914ab0b5b6dfcf0ea6
Tyler Whitehouse <whitehts@rose-hulman.edu>
2016-01-19 20:25:23 -0500

    Filling skeleton for UMLModifierManager for use in other classes

--- src/data/api/IUMLModifierManager.java
+++ src/data/api/IUMLModifierManager.java
@@ -1,5 +1,16 @@
 package data.api;
 
+import java.util.List;
+
+import data.impl.UMLCluster;
+
 public interface IUMLModifierManager {
 
+	public String getStyle(String className);
+	public void addStyle(String className, String style);
+	public String getSubtext(String className);
+	public void setSubtext(String className, String subtext);
+	public List<UMLCluster> getClusters();
+	
+	
 }
--- src/data/impl/UMLModifierManager.java
+++ src/data/impl/UMLModifierManager.java
@@ -1,7 +1,44 @@
 package data.impl;
 
+import java.util.List;
+import java.util.Map;
+
 import data.api.IUMLModifierManager;
 
 public class UMLModifierManager implements IUMLModifierManager {
+
+	private List<UMLCluster> clusters;
+	private Map<String, String> stylings;
+	private Map<String, String> subtexts;
+
+	@Override
+	public void addStyle(String className, String style) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String getSubtext(String className) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setSubtext(String className, String subtext) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<UMLCluster> getClusters() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getStyle(String className) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
