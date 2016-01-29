8c9900e56a4a58a1b6a9de8334e7dfa21db70f8e
Tyler Whitehouse <whitehts@rose-hulman.edu>
2016-01-28 23:44:32 -0500

    Updated adapterPatternFinder to use more accurate/restrictive conditions (SO MANY IFS!!!!)

--- src/pattern/impl/AdapterPatternFinder.java
+++ src/pattern/impl/AdapterPatternFinder.java
@@ -1,5 +1,6 @@
 package pattern.impl;
 
+import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -8,16 +9,17 @@ import java.util.Set;
 import pattern.api.IPatternFinder;
 import data.api.IClass;
 import data.api.IField;
+import data.api.IMethod;
 import data.api.IUMLModifierManager;
 
 public class AdapterPatternFinder implements IPatternFinder {
 
 	Set<String> whiteList;
 	
-	String style = "style=filled, fillcolor=red,";
-	String adapter = "\\<\\<Adapter\\>\\>";
-	String target = "\\<\\<Target\\>\\>";
-	String adaptee = "\\<\\<Adaptee\\>\\>";
+	private static final String style = "style=filled, fillcolor=red,";
+	private static final String adapterSub = "\\<\\<Adapter\\>\\>";
+	private static final String targetSub = "\\<\\<Target\\>\\>";
+	private static final String adapteeSub = "\\<\\<Adaptee\\>\\>";
 	
 	public AdapterPatternFinder(Set<String> whiteList){
 		this.whiteList = whiteList;
@@ -32,25 +34,51 @@ public class AdapterPatternFinder implements IPatternFinder {
 			Set<String> assoc = c.getAssoc();
 			assoc.addAll(c.getUses());
 			assoc.retainAll(whiteList);
-			if (fs.size() == 1 && impls.length == 1 && assoc.size() == 1){
+			
+			if (	fs.size() == 1 &&
+					impls.length == 1 &&
+					assoc.size() == 1){
+				
 				IField f = fs.get(0);
+				if (!f.isPrivate() && !f.isProtected()) continue;
+				
 				String fType = f.getType();
 				String implType = impls[0];
 				Iterator<String> assocIter  = assoc.iterator();;
 				String assocType = assocIter.next();
-				if (fType.contains(assocType)){
-					mm.addStyle(c.getName(), style);
-					mm.setSubtext(c.getName(), adapter);
+				
+				if (!fType.contains(c.getName())){
 					
-					mm.addStyle(implType, style);
-					mm.setSubtext(implType, target);
-					
-					mm.addStyle(assocType, style);
-					mm.setSubtext(assocType, adaptee);
+					Collection<IMethod> ms = c.getMethods();
+					Iterator<IMethod> msIter = ms.iterator();
+					IMethod m;
+					while (msIter.hasNext()){
+						m = msIter.next();
+						String name = m.getName();
+						if (name.equals("<init>")){
+							String[] args = m.getArguments();
+							if (args.length == 1){
+								String argType = args[0];
+								if (fType.contains(argType)){
+									this.styleClasses(mm, c.getName(), implType, assocType);
+								}
+							}
+						}
+					}
 				}
 			}
 		}
-		
 	}
 
+	private void styleClasses(IUMLModifierManager mm, String adapterClass, String targetClass, String adapteeSub){
+		mm.addStyle(adapterClass, AdapterPatternFinder.style);
+		mm.setSubtext(adapterClass, AdapterPatternFinder.adapterSub);
+		
+		mm.addStyle(targetClass, AdapterPatternFinder.style);
+		mm.setSubtext(targetClass, AdapterPatternFinder.targetSub);
+		
+		mm.addStyle(adapteeSub, AdapterPatternFinder.style);
+		mm.setSubtext(adapteeSub, AdapterPatternFinder.adapteeSub);
+	}
+	
 }
