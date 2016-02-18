206f4af4c76f0e0704649d8db6c4390a659687b5
Tyler Whitehouse <whitehts@rose-hulman.edu>
2016-02-18 09:51:13 -0500

    Functional GUI!!!! Sort of

--- src/gui/MainDisplayPanel.java
+++ src/gui/MainDisplayPanel.java
@@ -1,12 +1,11 @@
 package gui;
 
 import java.awt.BorderLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.Color;
+import java.awt.Dimension;
 
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
 public class MainDisplayPanel extends JPanel {
 
@@ -20,6 +19,18 @@ public class MainDisplayPanel extends JPanel {
 
 		this.setLayout(new BorderLayout());
 		
-		this.add(new SelectorPanel(),BorderLayout.WEST);
+		SelectorPanel sel = new SelectorPanel(this.parent.dmm.getList());
+		JScrollPane selScroll = new JScrollPane(sel);
+		
+		this.add(selScroll,BorderLayout.WEST);
+		Dimension dim = this.parent.getPreferredSize();
+		dim.width = dim.width / 3;
+		selScroll.setPreferredSize(dim);
+		
+		
+		JPanel p = new JPanel();
+		p.setBackground(Color.red);
+		JScrollPane pScroll = new JScrollPane(p);
+		this.add(pScroll, BorderLayout.CENTER);
 	}
 }
--- src/gui/SelectorPanel.java
+++ src/gui/SelectorPanel.java
@@ -1,46 +1,86 @@
 package gui;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.swing.BoxLayout;
 import javax.swing.JCheckBox;
 import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import data.api.IUMLModifier;
 
 public class SelectorPanel extends JPanel{
 
 	private static final long serialVersionUID = 1L;
 
-	private List<Object> list;
+	// connecting the two
+	private Map<JCheckBox, IUMLModifier> checkBoxToMod;
 	
-	private List<JCheckBox> checkBoxes;
-	
-	public SelectorPanel(){
-		this.list = new ArrayList<>();
-		this.checkBoxes = new ArrayList<>();
+	public SelectorPanel(List<IUMLModifier> list){
+		this.checkBoxToMod = new HashMap<>();
 		
 		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-	}
-	
-	public void setList(List<Object> newList){
-		this.list = newList;
 		
-		this.redoCheckBoxes();
+		this.repopCheckBoxes(list);
+		
 	}
 	
-	private void redoCheckBoxes(){
-		// remove from this
-		for (JCheckBox c : this.checkBoxes){
+	
+	
+	public void setList(List<IUMLModifier> list){
+		this.repopCheckBoxes(list);
+	}
+	
+	private void repopCheckBoxes(List<IUMLModifier> list){
+		// remove checkboxes
+		for (JCheckBox c : this.checkBoxToMod.keySet()){
 			this.remove(c);
 		}
-		this.checkBoxes.clear();
+		this.checkBoxToMod.clear();
 		
 		// add new ones
-		for (Object o : list){
-			String name = ""; // o.getDisplayName();
+		this.repopCheckBoxesHelper(list, 0);
+	}
+	
+	/**
+	 * 
+	 * @param l, The list to add
+	 * @param i, The indentation level
+	 */
+	private void repopCheckBoxesHelper(List<IUMLModifier> list, int i){
+		for (IUMLModifier m : list){
+			
+			String name = m.getDisplayName();
 			JCheckBox c = new JCheckBox(name);
-			checkBoxes.add(c);
+			
+			this.checkBoxToMod.put(c, m);
+			
+			c.setBorder(new EmptyBorder(0, 10*i, 0, 0));
+			c.addActionListener(new ActionListener(){
+				@Override
+				public void actionPerformed(ActionEvent arg0) {
+					m.setEnabled(c.isSelected());
+					SelectorPanel.this.updateCheckBoxStates();
+				}
+			});
+			
 			this.add(c);
+			
+			List<IUMLModifier> list2 = m.getList();
+			if (list2 != null && list2.size() > 0){
+				this.repopCheckBoxesHelper(list2,i+1);
+			}
+		}
+	}
+	
+	public void updateCheckBoxStates(){
+		for(JCheckBox c : this.checkBoxToMod.keySet()){
+			c.setSelected(this.checkBoxToMod.get(c).getEnabled());
 		}
 	}
 	
