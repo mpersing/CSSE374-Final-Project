83dfbe6430a9339d8504835e06c888ebb289246d
Tyler Whitehouse <whitehts@rose-hulman.edu>
2016-02-18 23:41:17 -0500

    Space doesn't crash running dot.exe

--- src/runners/CommandLineRunner.java
+++ src/runners/CommandLineRunner.java
@@ -8,7 +8,7 @@ public abstract class CommandLineRunner {
 		// Run the application if support is available
 		try {
 			System.out.format("Launching %s ...%n", command);
-			p = Runtime.getRuntime().exec("cmd /c start " + command + " " + arg);
+			p = Runtime.getRuntime().exec("cmd /c start \"\" " + command + " " + arg);
 			while(p.isAlive()) {
 				System.out.println("Waiting for termination..");
 				Thread.sleep(100);
